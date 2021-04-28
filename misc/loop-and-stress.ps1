862f63b47906b15f352bd49cd52e4cfcb6b0e396
Felipe De Bene <benfelip@amazon.com>
2021-04-28 11:20:09 -0500

    fixed scritp

--- misc/loop-and-stress.ps1
+++ misc/loop-and-stress.ps1
@@ -7,13 +7,13 @@
 $result = 1
 $number = 1
 $path="C:\logfile.log"
-Get-ItemProperty -Path HKLM:\SOFTWARE\benfelip -Name "CrazyLogs" 
-$a = Get-ItemPropertyValue 'HKLM:\SOFTWARE\benfelip' -Name CrazyLogs
+Get-ItemProperty -Path HKLM:\SOFTWARE\SampleApp -Name "CrazyLogs" 
+$a = Get-ItemPropertyValue 'HKLM:\SOFTWARE\SampleApp' -Name CrazyLogs
 
 While ($a -eq 1) {
     $result = $result * $number
     $number++
     $SampleString = "Added sample {0} at {1}" -f $result,(Get-Date).ToString("h:m:s")
     Add-Content -Path $path -Value $SampleString -Force
-    $a = Get-ItemPropertyValue 'HKLM:\SOFTWARE\benfelip' -Name CrazyLogs
+    $a = Get-ItemPropertyValue 'HKLM:\SOFTWARE\SampleApp' -Name CrazyLogs
 }
